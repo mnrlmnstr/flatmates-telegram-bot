abed71cd095238f34cff980e3311f1d8454896b5
Vlad Simankov <mnrlmnstr@gmail.com>
2023-03-08 00:36:02 +0200

    simplify replies

--- bot/ai.py
+++ bot/ai.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import openai
+from datetime import datetime
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -9,8 +10,11 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # taras 0.1: 'Your name is Taras. You are sad and cynic, make cringe jokes on user messages.'
 
+current_date = datetime.now().strftime("%A, %B %d, %Y %H:%M:%S")
 based_messages = [
-    {'role': 'system', 'content': 'Your name is Taras. You are sad and cynic, make cringe jokes on user messages.'},
+    {'role': 'system', 'content': 'Your name is Taras. You are ukrainian.'
+                                  'You are sad and cynic but do not take it seriously, make cringe jokes on user messages. '
+                                  f'Current date: {current_date}'},
 ]
 
 def generate_response(messages: list) -> str:
--- bot/weather.py
+++ bot/weather.py
@@ -3,64 +3,57 @@ import requests
 from bot.ai import generate_response
 from bot.translate import translate_text
 
-wmo_to_text = [
-    ([0],               'Clear sky'),
-    ([1, 2, 3],         'Mainly clear, partly cloudy, and overcast'),
-    ([45, 48],          'Fog and depositing rime fog'),
-    ([51, 53, 55],      'Drizzle: Light, moderate, and dense intensity'),
-    ([56, 57],          'Freezing Drizzle: Light and dense intensity'),
-    ([61, 63, 65],      'Rain: Slight, moderate and heavy intensity'),
-    ([66, 67],          'Freezing Rain: Light and heavy intensity'),
-    ([71, 73, 75],      'Snow fall: Slight, moderate, and heavy intensity'),
-    ([77],              'Snow grains'),
-    ([80, 81, 82],      'Rain showers: Slight, moderate, and violent'),
-    ([85, 86],          'Snow showers slight and heavy'),
-    ([95],              'Thunderstorm: Slight or moderate'),
-    ([96, 99],          'Thunderstorm with slight and heavy hail'),
-]
+wmo_to_emoji = {
+    0: '☀️',  # Clear sky
+    1: '🌤️',  # Mainly clear
+    2: '⛅',   # Partly cloudy
+    3: '☁️',   # Overcast
+    45: '🌫️',  # Fog and depositing rime fog
+    48: '🌁',   # Fog and depositing rime fog
+    51: '🌧️',  # Drizzle: Light
+    53: '🌧️',  # Drizzle: Moderate
+    55: '🌧️',  # Drizzle: Dense intensity
+    56: '❄️',   # Freezing Drizzle: Light
+    57: '❄️',   # Freezing Drizzle: Dense intensity
+    61: '🌧️',  # Rain: Slight
+    63: '🌧️',  # Rain: Moderate
+    65: '🌧️',  # Rain: Heavy intensity
+    66: '❄️',   # Freezing Rain: Light intensity
+    67: '❄️',   # Freezing Rain: Heavy intensity
+    71: '🌨️',  # Snow fall: Slight intensity
+    73: '🌨️',  # Snow fall: Moderate intensity
+    75: '🌨️',  # Snow fall: Heavy intensity
+    77: '🌨️',  # Snow grains
+    80: '🌧️',  # Rain showers: Slight intensity
+    81: '🌧️',  # Rain showers: Moderate intensity
+    82: '🌧️',  # Rain showers: Violent intensity
+    85: '🌨️',  # Snow showers: Slight intensity
+    86: '🌨️',  # Snow showers: Heavy intensity
+    95: '⛈️',  # Thunderstorm: Slight or moderate
+    96: '⛈️',  # Thunderstorm with slight hail
+    99: '⛈️',  # Thunderstorm with heavy hail
+}
 
 places = [
-    {'name': 'Kyiv', 'params': {'latitude': '50.45', 'longitude': '30.52', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
-    {'name': 'London', 'params': {'latitude': '51.51', 'longitude': '-0.13', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/London'}},
-    {'name': 'Rotterdam', 'params': {'latitude': '51.92', 'longitude': '4.48', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
-    {'name': 'Groningen', 'params': {'latitude': '53.22', 'longitude': '6.57', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
-    {'name': 'Berlin', 'params': {'latitude': '52.52', 'longitude': '13.41', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
-    {'name': 'Kraków', 'params': {'latitude': '50.06', 'longitude': '19.94', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
-    {'name': 'Prague', 'params': {'latitude': '50.09', 'longitude': '14.42', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
+    {'name': '🇺🇦 Київ             ', 'params': {'latitude': '50.45', 'longitude': '30.52', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
+    {'name': '🇬🇧 Лондон       ', 'params': {'latitude': '51.51', 'longitude': '-0.13', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/London'}},
+    {'name': '🇳🇱 Ротердам   ', 'params': {'latitude': '51.92', 'longitude': '4.48', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
+    {'name': '🇳🇱 Гронінген   ', 'params': {'latitude': '53.22', 'longitude': '6.57', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
+    {'name': '🇩🇪 Берлін         ', 'params': {'latitude': '52.52', 'longitude': '13.41', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
+    {'name': '🇵🇱 Краків         ', 'params': {'latitude': '50.06', 'longitude': '19.94', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
+    {'name': '🇨🇿 Прага          ', 'params': {'latitude': '50.09', 'longitude': '14.42', 'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min'], 'timezone': 'Europe/Berlin'}},
 ]
-
-
-def text_by_wmo(code):
-    for wmo in wmo_to_text:
-        if code in wmo[0]:
-            return wmo[1]
 
 
 def get_forecast(params: dict):
-    """Get meteo gorecast from Open Meteo for today"""
     url = 'https://api.open-meteo.com/v1/forecast/'
     r = requests.get(url, params=params)
-    if r.status_code == 200:
-        return r.json()
-    else:
-        return r.status_code
+    return r.json() if r.status_code == 200 else r.status_code
 
 
 def forecast_text():
-    text = ''
+    forecast = ''
     for place in places:
         fc = get_forecast(place.get('params'))['daily']
-        text += f"{place.get('name')} H:{round(fc['temperature_2m_max'][0])}° L:{round(fc['temperature_2m_min'][0])}°" \
-                f" {text_by_wmo(fc['weathercode'][0])}."
-
-    messages = [
-        {'role': 'system', 'content': 'Your a weather reporter and comedian. '
-                                      'Highlight cities by country emoji and format title on separated line. '
-                                      'Highlight weather condition by emoji. '
-                                      'Make joke about every city forecast.'},
-        {'role': 'user', 'content': f'Forecast data: {text}'},
-    ]
-
-    generated_text = generate_response(messages)
-    translated_text = translate_text(generated_text)
-    return translated_text
+        forecast += f"{place.get('name')} {wmo_to_emoji[fc['weathercode'][0]]}   H:{round(fc['temperature_2m_max'][0])}° L:{round(fc['temperature_2m_min'][0])}° \n"
+    return forecast
