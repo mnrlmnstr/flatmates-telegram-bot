94aa327e12b7a51bf46503f4e0dfad3f0c143223
Vlad Simankov <mnrlmnstr@gmail.com>
2023-03-08 20:42:33 +0200

    add war chart

--- bot/war_stats.py
+++ bot/war_stats.py
@@ -1,30 +1,40 @@
 import os
+import datetime as dt
 
 import requests
-# import datetime
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-# ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-#
-# def war_chart():
-#     url = 'https://russianwarship.rip/api/v2/statistics'
-#     now = datetime.datetime.now()
-#     month_ago = now - datetime.timedelta(days=30)
-#
-#     r = requests.get(url, params={'date_from': month_ago.strftime("%Y-%m-%d"), 'date_to': now.strftime("%Y-%m-%d")})
-#     stats = r.json()['data']['records']
-#     x = [record['date'] for record in stats]
-#     y = [record['increase']['personnel_units'] for record in stats]
-#
-#     fig, ax = plt.subplots()
-#     ax.plot(x, y)
-#     ax.set_xlabel('Час')
-#     ax.set_ylabel('Росіяни')
-#     ax.set_title('Війна')
-#
-#     tmp_dir = os.path.join(ROOT_DIR, 'tmp')
-#     plt.savefig(tmp_dir + '/war_chart.png')
-#     plt.close()
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def war_chart():
+    url = 'https://russianwarship.rip/api/v2/statistics'
+    now = dt.datetime.now()
+    date_from = now - dt.timedelta(days=7)
+
+    r = requests.get(url, params={'date_from': date_from.strftime("%Y-%m-%d"), 'date_to': now.strftime("%Y-%m-%d")})
+    stats = r.json()['data']['records']
+    x = [record['date'] for record in stats]
+    x = [dt.datetime.strptime(date, '%Y-%m-%d') for date in x]
+    y = [record['increase']['personnel_units'] for record in stats]
+
+    img = plt.imread(os.path.join(ROOT_DIR, 'tmp/zal.jpg'))
+    fig, ax = plt.subplots()
+    fig.figimage(img, 0, 0, alpha=0.6, resize=True)
+    ax.bar(x, y, color='red')
+
+    date_format = mdates.DateFormatter('%b %d')
+    ax.xaxis.set_major_formatter(date_format)
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+
+    ax.set_ylabel('Втрати')
+    ax.set_title('Тиждень росіян')
+
+    tmp_dir = os.path.join(ROOT_DIR, 'tmp')
+    plt.savefig(tmp_dir + '/war_chart.png')
+    plt.close()
+
 
 def get_war_stats():
     """Get latest war stats"""
